0b9afa32235d2437d6a482a54a94e3fdec81555f
June Kim <juneyk1@uci.edu>
2024-10-21 00:32:23 -0700

    File upload

---
+++ frontend/src/types.ts
@@ -0,0 +1,13 @@
+export interface Recipe {
+    id: number;
+    title: string;
+    image: string;
+    imageType: string;
+}
+
+export interface RecipeSummary {
+    id: number,
+    title: string
+    summary: string
+    
+}
